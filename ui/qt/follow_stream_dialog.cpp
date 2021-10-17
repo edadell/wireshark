4c62533af7cd6387d13655e3331b47d9f8cb0fbc
Eugene Adell <eugene.adell@gmail.com>
2021-10-17 11:37:31 +0200

    Ensure a single Follow Stream is running only
    When changing one of the selection parameters in the Follow Stream
    dialog while a filtering task is already running, the result is
    inaccurate.
    While a filtering task is already running in the Follow Stream
    dialog, any filter change which triggers a new filtering gives a
    wrong result. Both the displayed data and the Save As functions
    are impacted. Closes #15637

--- ui/qt/follow_stream_dialog.cpp
+++ ui/qt/follow_stream_dialog.cpp
@@ -47,6 +47,7 @@
 #include <QElapsedTimer>
 #include <QKeyEvent>
 #include <QMessageBox>
+#include <QMutex>
 #include <QPrintDialog>
 #include <QPrinter>
 #include <QScrollBar>
@@ -62,6 +63,12 @@
 
 // Matches SplashOverlay.
 static int info_update_freq_ = 100;
+
+// Handle the loop breaking notification properly
+static QMutex loop_break_mutex;
+
+// Indicates that a Follow Stream is currently running
+static gboolean isReadRunning_;
 
 FollowStreamDialog::FollowStreamDialog(QWidget &parent, CaptureFile &cf, follow_type_t type) :
     WiresharkDialog(parent, cf),
@@ -543,6 +550,11 @@ void FollowStreamDialog::resetStream()
 frs_return_t
 FollowStreamDialog::readStream()
 {
+
+    // interrupt any reading already running
+    loop_break_mutex.lock();
+    isReadRunning_ = FALSE;
+    loop_break_mutex.unlock();
 
     ui->teStreamContent->clear();
     text_pos_to_packet_.clear();
@@ -1215,8 +1227,12 @@ FollowStreamDialog::readFollowStream()
 
     elapsed_timer.start();
 
+    loop_break_mutex.lock();
+    isReadRunning_ = TRUE;
+    loop_break_mutex.unlock();
+
     for (cur = g_list_last(follow_info_.payload); cur; cur = g_list_previous(cur)) {
-        if (dialogClosed()) break;
+        if (dialogClosed() || !isReadRunning_) break;
 
         follow_record = (follow_record_t *)cur->data;
         skip = FALSE;
@@ -1254,6 +1270,10 @@ FollowStreamDialog::readFollowStream()
             }
         }
     }
+
+    loop_break_mutex.lock();
+    isReadRunning_ = FALSE;
+    loop_break_mutex.unlock();
 
     return FRS_OK;
 }
